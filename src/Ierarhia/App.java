1e878799efcebd2b3fece1a921ed10b9f801df89
Roger <rogernuica3@gmail.com>
2021-01-21 00:40:44 +0200

    Implemented functionalities with SQL Queries

--- src/Ierarhia/App.java
+++ src/Ierarhia/App.java
@@ -1,5 +1,11 @@
 package Ierarhia;
 
+import sqlite.Connect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class App {
@@ -9,7 +15,7 @@ public class App {
     }
 
     public void afiseaza_farmacii_cu_medicament_pe_stoc(Medicament medicament_anume) {
-        for(Oras oras: lista_orase) {
+       /* for(Oras oras: lista_orase) {
             for(Farmacie farmacie: oras.getFarmacii() ) {
                 for(Medicament medicament: farmacie.getStoc()){
                     if (medicament.equals(medicament_anume))
@@ -17,5 +23,41 @@ public class App {
                 }
             }
         }
+        */
+        for(Oras oras: lista_orase) {
+
+            Connection conn = Connect.connect("database.db");
+            ResultSet rs = null;
+
+            String sql_command = "SELECT * FROM farmacii WHERE  stoc = " + "\'" + medicament_anume.getNume() + "\' and " +
+                    "nume=" + "\'" + oras.getNume_Oras() + "\'";
+
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(sql_command);
+                rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+
+                    String nume_oras = rs.getString("nume");
+                    String sql_command2 = "SELECT * FROM oras WHERE  stoc = " + "\'" + medicament_anume.getNume() + "\'";
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                Connect.closeConnection();
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void afiseaza_farmacii_pentru_un_oras_anume(Oras oras){
+        oras.afiseaza_farmacii_pentru_oras();
+    }
+
+    public void afiseaza_cantitate_totala_de_medicamente_pentru_un_oras_anume(Medicament medicament, Oras oras){
+        oras.afiseaza_cantitate_totala_de_medicamente_oras(medicament);
     }
 }
